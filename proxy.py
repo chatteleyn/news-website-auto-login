841d334256e3add2708341b9fab9717a61cbab6a
Lucas Chatteleyn <lucas.chatteleyn@gmail.com>
2024-06-11 00:57:05 +0200

    Update proxy.py

--- proxy.py
+++ proxy.py
@@ -9,7 +9,7 @@ import os
 EMAIL = os.environ['EMAIL']
 PASSWORD = os.environ['PASSWORD']
 
-XPATH_RE = "xpath\((.*)\)"
+XPATH_RE = r"xpath\((.*)\)"
 
 HEADERS = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/116.0"}
 
