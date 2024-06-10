46d8b08dfa9bdda1d4d43af484bb187c3878b97e
Lucas Chatteleyn <lucas.chatteleyn@gmail.com>
2024-06-11 00:02:04 +0200

    Update proxy.py

--- proxy.py
+++ proxy.py
@@ -4,10 +4,37 @@ import requests
 from lxml import etree
 from flask import Flask, jsonify, request
 import re
+import os
+
+EMAIL = print(os.environ['EMAIL'])
+PASSWORD = print(os.environ['PASSWORD'])
 
 XPATH_RE = "xpath\((.*)\)"
 
 HEADERS = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/116.0"}
+
+LOGINS = {
+    "www.mediapart.fr": {
+        "login_url": "https://www.mediapart.fr/login_check",
+        "login": {"email": EMAIL, "password": PASSWORD},
+        "not_logged_in": "xpath(//div[contains(@class, 'paywall-login')])",
+    },
+    "www.monde-diplomatique.fr": {
+        "login_url": "https://www.monde-diplomatique.fr/connexion/",
+        "login": {
+            "email": EMAIL,
+            "mot_de_passe": PASSWORD,
+            "valider": "Se+connecter",
+            "session_remember": "oui",
+            "formulaire_action": "identification_lecteur_ws",
+            "retour": "https://www.monde-diplomatique.fr/",
+            "email_nobot": "",
+            "formulaire_action_args": "xpath(//form//input[@name='formulaire_action_args']/@value)",
+            "_jeton": "xpath(//form//input[@name='_jeton']/@value)",
+        },
+        "not_logged_in": "xpath(//div[@id='paywall'])",
+    },
+}
 
 app = Flask(__name__)
 
@@ -16,35 +43,29 @@ session = requests.Session()
 
 @app.route("/fetch", methods=["GET"])
 def fetch_url_content():
-    return """<!doctype html>
-<html>
-<head>
-<META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">
-<meta charset="utf-8">
+    url = request.args.get("url")
+    if not url:
+        return jsonify({"error": "URL parameter is required"}), 400
 
-<title>Original article title</title>
-</head>
+    key = urlparse(url).netloc
 
-<body>
-<article class="film_review">
-<header>
-<h2>some subtitle</h2>
-</header>
-<section class="main_review">
-<p>copy of article I want to read</p>
-</section>
+    response = session.get(url, headers=HEADERS)
 
-<footer>
-<p>
-Posted on
-<time datetime="2015-05-15 19:00">May 15</time>
-by Staff.
-</p>
-</footer>
-</article>
-</body>
-</html>"""
+    if key in LOGINS and etree.HTML(response.content).xpath(re.search(XPATH_RE, LOGINS[key]["not_logged_in"]).group(1)):
+        tree = None
+        login = LOGINS[key]["login"]
+
+        for field in login.keys():
+            if re.search(XPATH_RE, login[field]):
+                if tree is None:
+                    tree = etree.HTML(session.get(LOGINS[key]["login_url"], headers=HEADERS).content)
+                login[field] = tree.xpath(re.search(XPATH_RE, login[field]).group(1))
+
+        session.post(LOGINS[key]["login_url"], data=login, headers=HEADERS)
+        response = session.get(url, headers=HEADERS)
+
+    return response.text
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run()
