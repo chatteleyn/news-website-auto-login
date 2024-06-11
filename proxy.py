28f2f539b5822c966992d4607adf8352c63013a7
Lucas Chatteleyn <lucas.chatteleyn@gmail.com>
2024-06-11 09:43:30 +0200

    Update proxy.py

--- proxy.py
+++ proxy.py
@@ -10,7 +10,7 @@ from lxml import etree
 EMAIL = "***REMOVED***"
 PASSWORD = "***REMOVED***"
 
-XPATH_RE = r"xpath\((.*)\)"
+XPATH_RE = "xpath\((.*)\)"
 
 HEADERS = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/116.0"}
 
